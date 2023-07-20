ed07df0e6f922bf6af5d56aaa06211ca85123c1f
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2023-07-20 20:52:57 +0900

    Fix config_wsl.ps1 for cpuInfo

--- config_wsl.ps1
+++ config_wsl.ps1
@@ -1,22 +1,22 @@
 function  Check-SysInfo {
     $sysInfo = Get-CimInstance Win32_ComputerSystem
-    $totalRam = $sysInfo.TotalPhysicalMemory / 1GB
+    $totalRam = [Math]::Ceiling($sysInfo.TotalPhysicalMemory / 1GB)
 
     $cpuInfo = Get-CimInstance Win32_Processor
-    $cpuCore = $processor.NumberOfCores
+    $cpuCore = $cpuInfo.NumberOfCores
 
     return $cpuCore, $totalRam
 }
 
 function GenerateFile($configPath, $configCPU, $configRam) {
-    $configContents = "`n[wsl2]`nmemory=$totalRam`nprocessors=$configCPU`nswap=$configRam"
+    $configContents = "`n[wsl2]`nmemory=$totalRam`nprocessors=$configCPU\GB`nswap=$configRam\GB"
     $configContents | Out-File $configPath -Encoding utf8
 }
 
 function Configure-WSL($confOption, $cpuCore, $totalRam) {
     $configFile = "$env:USERPROFILE\.wslconfig"
     $configFileBck = "$env:USERPROFILE\.wslconfig.bck"
-
+    
     if(Test-Path $configFile) {
         Move-Item -Path $configFile -Destination $configFileBck -Force
     }
@@ -36,7 +36,7 @@ function main {
     Write-Host "`nSet4wsl`n - Configuration WSL"
 
     $cpuCore, $totalRam = Check-SysInfo
-    Write-Host "System Information`n • CPU: $cpuCore Core`n • RAM: $totalRam GB`n"
+    Write-Host "System Information`n - CPU: $cpuCore Core`n - RAM: $totalRam GB`n"
 
     Write-Host "Set4wsl`n  1) Everything  Share`n  2) High Performance`n  3) Balance Control`n  4) Limited Resource"
     $confOption = Read-Host "Choose option:  "
